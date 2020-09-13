b2ad050a44148da7769878ecf5a4863403c33632
Jim Perusse <67986763+jperusse@users.noreply.github.com>
2020-09-13 12:16:07 -0700

    Added an extra testcase

--- src/first_test.py
+++ src/first_test.py
@@ -5,5 +5,8 @@ class FirstTestClass(unittest.TestCase):
     def test_upper(self):
         self.assertEqual('rubiks code'.upper(), 'RUBIKS CODE')
         
+    def test_lower(self):
+        self.assertEqual('RUBIKS CODE'.lower(), 'rubiks code')
+        
 if __name__ == '__main__':
     unittest.main()
